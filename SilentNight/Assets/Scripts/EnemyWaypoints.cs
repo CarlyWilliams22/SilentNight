5c7d2310ce72b85fab3fe0c485aede0e4a24cafd
Zonic <joshuaisawesome98@yahoo.com>
2020-10-14 19:29:40 -0400

    Enemy Movement
    Loaded a asset that uses pathfinding for the sound monster in the caves

---
+++ SilentNight/Assets/Scripts/EnemyWaypoints.cs
@@ -0,0 +1,21 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWaypoints : MonoBehaviour
+{
+    AIDestinationSetter test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        test.
+    }
+}
