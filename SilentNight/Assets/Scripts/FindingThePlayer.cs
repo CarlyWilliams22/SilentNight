3f43ae22c28d39222df3d3100d2d93df81618c77
Zonic <joshuaisawesome98@yahoo.com>
2020-10-15 15:42:04 -0400

    Improved Monster Movement
    Added monster able to patrol a route
    When player is in monsters radius, monster will follow players last position in radius, if player leaves the radius, the monster returns to its patrol.

---
+++ SilentNight/Assets/Scripts/FindingThePlayer.cs
@@ -0,0 +1,30 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindingThePlayer : MonoBehaviour
+{
+    private Patrol patrolScript;
+    private AIDestinationSetter destinationScript;
+
+    public int foundPlayer = 0;
+
+    private void Start()
+    {
+        patrolScript = this.transform.parent.gameObject.GetComponent<Patrol>();
+        destinationScript = this.transform.parent.GetComponent<AIDestinationSetter>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        patrolScript.enabled = !patrolScript.enabled;
+        destinationScript.enabled = !destinationScript.enabled;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        patrolScript.enabled = !patrolScript.enabled;
+        destinationScript.enabled = !destinationScript.enabled;
+    }
+}
