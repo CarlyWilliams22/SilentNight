97668ae0a60da8263fdc310f989f3d816a12c445
dreigle <70277016+dreigle@users.noreply.github.com>
2020-10-15 21:30:28 -0400

    Added Player Movement

---
+++ SilentNight/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D rbody;
+
+    public int walkSpeed;
+    public int runSpeed;
+    public int sneakSpeed;
+    int curSpeed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        curSpeed = walkSpeed; //start out walking
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if(curSpeed == runSpeed)
+            {
+                curSpeed = walkSpeed;
+            }
+            else
+            {
+                curSpeed = runSpeed;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (curSpeed == sneakSpeed)
+            {
+                curSpeed = walkSpeed;
+            }
+            else
+            {
+                curSpeed = sneakSpeed;
+            }
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 vel = new Vector2(x, y);
+        rbody.velocity = curSpeed * vel.normalized;
+    }
+}
