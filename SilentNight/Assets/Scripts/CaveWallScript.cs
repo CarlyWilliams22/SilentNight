16fd84fb72209d31c1b9f791a9cd9616739aab5c
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2020-10-15 23:00:58 -0400

    Soundwave Progress

---
+++ SilentNight/Assets/Scripts/CaveWallScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveWallScript : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+    float a, r, g, b;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        a = 0;
+        r = sr.color.r;
+        g = sr.color.g;
+        b = sr.color.b;
+        sr.color = new Color(r, g, b, a);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (a > 0)
+        {
+            a -= (float).001;
+            sr.color = new Color(r, g, b, a);
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Soundwave"))
+        {
+            a = 1;
+            sr.color = new Color(r, g, b, a);
+        }
+        
+    }
+}
---
+++ SilentNight/Assets/Scripts/SoundwaveScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundwaveScript : MonoBehaviour
+{
+
+    Transform transform;
+    SpriteRenderer sr;
+    float xScale, yScale;
+    float clear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = gameObject.GetComponent<Transform>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        xScale = yScale = 0;
+        clear = sr.color.a;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(xScale, xScale, 0);
+        xScale = yScale += (float).01;
+        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, clear);
+        clear -= (float).001;
+        if(clear < .0000001)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
