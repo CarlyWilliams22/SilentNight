42043e66eacb15ff377dd686c191ee4383c93b7b
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2020-10-16 20:34:24 -0400

    Updates to Soundwave

---
+++ SilentNight/Assets/Scripts/STMScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class STMScript : MonoBehaviour
+{
+    public SoundwaveScript soundwavePrototype;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            sneakSoundwave();
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            walkSoundwave();
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            runSoundwave();
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            clapSoundwave();
+        }
+    }
+
+    void sneakSoundwave()
+    {
+        SoundwaveScript s = Instantiate(soundwavePrototype, new Vector2(0, 0), Quaternion.identity);
+        s.setSize(1);
+    }
+
+    void walkSoundwave()
+    {
+        SoundwaveScript s = Instantiate(soundwavePrototype, new Vector2(0, 0), Quaternion.identity);
+        s.setSize((float).45);
+    }
+
+    void runSoundwave()
+    {
+        SoundwaveScript s = Instantiate(soundwavePrototype, new Vector2(0, 0), Quaternion.identity);
+        s.setSize((float).175);
+    }
+
+    void clapSoundwave()
+    {
+        SoundwaveScript s = Instantiate(soundwavePrototype, new Vector2(0, 0), Quaternion.identity);
+        s.setSize((float).1);
+    }
+}
--- SilentNight/Assets/Scripts/SoundwaveScript.cs
+++ SilentNight/Assets/Scripts/SoundwaveScript.cs
@@ -9,6 +9,7 @@ public class SoundwaveScript : MonoBehaviour
     SpriteRenderer sr;
     float xScale, yScale;
     float clear;
+    float scale;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +17,27 @@ public class SoundwaveScript : MonoBehaviour
         sr = gameObject.GetComponent<SpriteRenderer>();
         xScale = yScale = 0;
         clear = sr.color.a;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localScale = new Vector3(xScale, xScale, 0);
-        xScale = yScale += (float).01;
-        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, clear);
-        clear -= (float).001;
-        if(clear < .0000001)
-        {
-            Destroy(gameObject);
-        }
+        
+            xScale = yScale += (float).01;
+            transform.localScale = new Vector3(xScale, yScale, 0);
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, clear);
+            clear -= (float).01 * scale;
+            if (clear < .0000001)
+            {
+                Destroy(gameObject);
+            }
+        
+        
+    }
+
+    public void setSize(float size)
+    {
+        scale = size;
     }
 }
