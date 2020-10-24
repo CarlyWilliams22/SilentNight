50f4ed47c5c9798300f9bf4efd8091973b3f6ca8
JoshuaMcNamara98 <72767409+JoshuaMcNamara98@users.noreply.github.com>
2020-10-24 16:39:08 -0400

    Game Over Screen

--- SilentNight/Assets/Scripts/Level1ManagerScript.cs
+++ SilentNight/Assets/Scripts/Level1ManagerScript.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Level1ManagerScript : MonoBehaviour
 {
+    private Scene currentScene;
+    public GameObject canvas;
     public PlayerScript player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvas.SetActive(false);
+        currentScene = SceneManager.GetActiveScene();
     }
 
     // Update is called once per frame
@@ -18,12 +23,24 @@ public class Level1ManagerScript : MonoBehaviour
     {
         if (!player)
         {
-            ReloadScene();
+            canvas.SetActive(true);
         }
     }
 
-    private void ReloadScene()
+    public void LoadMainMenu()
     {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void RetryLevel()
+    {
+        if (currentScene.name.Equals("Level1"))
+        {
+            SceneManager.LoadScene("Level1");
+        }
+        else if (currentScene.name.Equals("Level2"))
+        {
+            SceneManager.LoadScene("Level2");
+        }
     }
 }
