815e4b1b4ed222dafd7dc65d1f3e01cb2876ebcc
Zonic <joshuaisawesome98@yahoo.com>
2020-11-21 22:02:43 -0500

    Boss Updates

---
+++ SilentNight/Assets/Scripts/FinalBossScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBossScript : MonoBehaviour
+{
+    public Transform player;
+
+    private void Update()
+    {
+        Vector3 vectorToTarget = player.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle + 180f, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, 1f);
+    }
+}
