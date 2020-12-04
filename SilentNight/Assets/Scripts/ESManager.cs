0b7c03ebf87ffbbb25756b08b8a18a460e0da893
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2020-12-04 17:55:58 -0500

    Final achievements

--- SilentNight/Assets/Scripts/ESManager.cs
+++ SilentNight/Assets/Scripts/ESManager.cs
@@ -6,6 +6,33 @@ using UnityEngine.SceneManagement;
 public class ESManager : MonoBehaviour
 {
 
+    public GameObject achievementBox, achievement3txt, achievement6txt;
+    private void Start()
+    {
+        //PlayerPrefs.SetInt("Trophy3", 0);
+        PlayerPrefs.SetInt("Trophy6", 0);
+
+        if (PlayerPrefs.GetInt("Trophy3") == 0)
+        {
+            PlayerPrefs.SetInt("Trophy3", 1);
+            achievementBox.SetActive(true);
+            achievement3txt.SetActive(true);
+            if((PlayerPrefs.GetInt("Trophy6")==0) && (PlayerPrefs.GetInt("Lives") == 5))
+            {
+                PlayerPrefs.SetInt("Trophy6", 1);
+                Invoke("Trophy6Wait", 3);
+            }
+        }
+
+        if((PlayerPrefs.GetInt("Trophy6") == 0) && (PlayerPrefs.GetInt("Lives") == 5))
+        {
+            PlayerPrefs.SetInt("Trophy6", 1);
+            achievementBox.SetActive(true);
+            achievement6txt.SetActive(true);
+
+        }
+    }
+
     public void LoadMenu()
     {
         SceneManager.LoadScene("MainMenu");
@@ -14,5 +41,12 @@ public class ESManager : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    void Trophy6Wait()
+    {
+        achievementBox.SetActive(false);
+        achievementBox.SetActive(true);
+        achievement6txt.SetActive(true);
     }
 }
